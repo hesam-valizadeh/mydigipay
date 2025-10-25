fb2dd37b123e8c7d22b4a55cc416e0f2527b882c
Heasam Valizadeh <H.valizadeh@son.ir>
2025-10-25 16:57:09 +0330

    add

--- src/app/@layout/header/header.component.ts
+++ src/app/@layout/header/header.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {RouterLinksPath} from '../../@core/constans/router-links';
 import {RouterLink} from '@angular/router';
 import { SearchBoxComponent } from '../../@shared/components/search-box/search-box.component';
 import { HeaderViewDataInterface } from '../../view-models/header-view-data.interface';
+import { ResponsiveService } from '../../@core/services/responsive.service';
 
 @Component({
   selector: 'app-header',
@@ -15,7 +16,7 @@ import { HeaderViewDataInterface } from '../../view-models/header-view-data.inte
 export class HeaderComponent {
 
   protected readonly RouterLinksPath = RouterLinksPath;
-
+  responsive = inject(ResponsiveService);
   data: HeaderViewDataInterface = {
     
   }
