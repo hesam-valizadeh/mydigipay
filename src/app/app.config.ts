4f6931126191fbae5913e215c84723e72eed6353
Heasam Valizadeh <H.valizadeh@son.ir>
2025-11-02 07:04:34 +0330

    added mock data and add interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,12 +8,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { MockApiInterceptor } from './@core/interceptors/mock-api.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([MockApiInterceptor])
+    ),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
