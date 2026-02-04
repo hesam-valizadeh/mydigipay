bf0170f4fc6a29c9b479a66609cf33d6b6ad84f5
hesam-valizadeh <hesam.valizadeh@gmail.com>
2026-02-05 00:58:45 +0330

    .

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,8 @@ import { AppComponent } from './app/app.component';
 import { register as registerSwiperElements } from 'swiper/element/bundle';
 registerSwiperElements();
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+if ('serviceWorker' in navigator ) {
+    navigator.serviceWorker.register('ngsw-worker.js')
+      .then(reg => console.log('Service Worker Registered'))
+      .catch(err => console.error('Service Worker Registration Failed', err));
+  }
