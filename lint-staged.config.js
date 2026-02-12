73f6032303838850d0225b3f278abdbd9673620d
Hesam Valizadeh <hesam.valizadeh@gmail.com>
2026-02-12 22:53:04 +0330

    fix: fix

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   '**/*.{ts,tsx}': 'eslint --fix',
   '**/*.{scss,css}': [
+    'stylelint --fix', // بعد lint 
     'postcss --config postcss.config.js --replace', // اضافه کردن autoprefixer
-    'stylelint --fix', // بعد lint
   ],
   '**/*.html': 'prettier --write',
   '**/*.{json,md,yml}': 'prettier --write',
