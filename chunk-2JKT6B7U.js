117b4b705266c25b48f9a93c879cf46683714d83
hesam-valizadeh <hesam-valizadeh@users.noreply.github.com>
2026-02-12 16:08:34 +0000

    deploy: fc43793727198dff265f108894b1fd6ec089a0af

---
+++ chunk-2JKT6B7U.js
@@ -0,0 +1,1 @@
+import{Sa as n}from"./chunk-R65OAF3O.js";var p=(()=>{class e{trackEffect(){}static \u0275fac=function(t){return new(t||e)};static \u0275cmp=n({type:e,selectors:[["app-wealth"]],decls:0,vars:0,template:function(t,a){},encapsulation:2,changeDetection:0})}return e})();export{p as WealthComponent};
