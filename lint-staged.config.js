4d6429067464353b3778391ae0ec68d99673f84c
Hesam Valizadeh <hesam.valizadeh@gmail.com>
2026-02-14 22:16:49 +0330

    feat: pWA

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,18 +1,6 @@
 module.exports = {
-  '**/*.{ts,tsx}': [
-    'eslint --fix',
-    'eslint'
-  ],
-  '**/*.{scss,css}': [
-    'stylelint --fix',
-    'stylelint'
-  ],
-  '**/*.html': [
-    'prettier --write',
-    'prettier --check'
-  ],
-  '**/*.{json,md,yml}': [
-    'prettier --write',
-    'prettier --check'
-  ],
+  '**/*.{ts,tsx}': ['eslint --fix', 'eslint'],
+  '**/*.{scss,css}': ['stylelint --fix', 'stylelint'],
+  '**/*.html': ['prettier --write', 'prettier --check'],
+  '**/*.{json,md,yml}': ['prettier --write', 'prettier --check'],
 };
