c60ce121311a2bf9cb41cf8d7e7590091d12fad2
hesam-valizadeh <hesam-valizadeh@users.noreply.github.com>
2026-02-18 07:24:25 +0000

    deploy: 97d0fa1588920787ebfad66e3a0457f6f81447fd

---
+++ chunk-MSCHY3NF.js
@@ -0,0 +1,2 @@
+import{Sa as n}from"./chunk-PUPZVWDX.js";var p=(()=>{class e{trackEffect(){}static \u0275fac=function(t){return new(t||e)};static \u0275cmp=n({type:e,selectors:[["app-hub"]],decls:0,vars:0,template:function(t,c){},encapsulation:2,changeDetection:0})}return e})();export{p as HubComponent};
+//# sourceMappingURL=chunk-MSCHY3NF.js.map
