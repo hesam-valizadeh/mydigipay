b91f89d03e79668a6a1768a651407bb6287604c3
hesam-valizadeh <hesam-valizadeh@users.noreply.github.com>
2026-02-18 22:54:55 +0000

    deploy: 4680db5b7792df73447ca08a7d6305dc97b7dd20

--- chunk-RIUWGA6S.js
+++
@@ -1,2 +0,0 @@
-import{Sa as n}from"./chunk-PUPZVWDX.js";var p=(()=>{class e{trackEffect(){}static \u0275fac=function(t){return new(t||e)};static \u0275cmp=n({type:e,selectors:[["app-wealth"]],decls:0,vars:0,template:function(t,a){},encapsulation:2,changeDetection:0})}return e})();export{p as WealthComponent};
-//# sourceMappingURL=chunk-RIUWGA6S.js.map
