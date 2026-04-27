bd44177acb577f95f9f0b5c97d1630861a67bdca
hesam-valizadeh <hesam-valizadeh@users.noreply.github.com>
2026-04-27 20:36:38 +0000

    deploy: 5387637d564836c755a0ef8f55d94de4cde7bc25

---
+++ chunk-V4GU37NN.js
@@ -0,0 +1,20 @@
+import{b as p}from"./chunk-KUJU6QC3.js";import"./chunk-X4AUF4LW.js";import{Db as i,Sa as a,mb as e,nb as t,ob as n}from"./chunk-G5C3WEJE.js";var g=(()=>{class o{slider=new p([{id:1,image:"assets/images/01.webp"}]);SwiperConfig={slidesPerView:"auto",spaceBetween:15,centeredSlides:!1,grabCursor:!0,loopPreventsSliding:!1,loop:!0,autoplay:{delay:5e3,disableOnInteraction:!1},pagination:{type:"bullets"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},injectStyles:[`
+         .swiper-pagination-bullet {
+        background-color: #C9CBCF !important;
+        opacity: 1;
+        width: 6px;
+        height: 6px;
+        border-radius: 30px;
+        margin: 0 4px;
+    }
+
+    .swiper-pagination-bullet.swiper-pagination-bullet-active {
+        width: 14px !important;
+        background-color: #fff !important;
+
+    }
+
+
+`],breakpoints:{}};static \u0275fac=function(r){return new(r||o)};static \u0275cmp=a({type:o,selectors:[["app-services"]],decls:23,vars:0,consts:[[1,"hub-page"],[1,"hub-header"],[1,"container"],[1,"d-flex","align-items-center","flex-nowrap","justify-content-between"],[1,"icon","icon-primary","icon-scan-linear","fs-24px","text-white"],[1,"d-flex","align-items-center","gap-4px"],[1,"fs-12px","fw-800","text-white"],[1,"icon","icon-primary","icon-eye-linear","text-white","fs-16px"],[1,"icon","icon-notification-linear","icon-primary","fs-24px","text-white"],[1,"d-flex","align-items-center","justify-content-center","gap-6px","mt-3"],[1,"fs-32px","text-white"],[1,"fs-18px","text-white"],[1,"d-flex","align-items-center","justify-content-center","gap-6px","mt-1"],[1,"fs-10px","text-white"],[1,"icon","icon-info-circle-linear","icon-primary","fs-16px","text-white"]],template:function(r,m){r&1&&(e(0,"div",0)(1,"div",1)(2,"div",2)(3,"div",3)(4,"div"),n(5,"i",4),t(),e(6,"div",5)(7,"span",6),i(8,"\u0645\u0648\u062C\u0648\u062F\u06CC \u06A9\u0644"),t(),e(9,"span"),n(10,"i",7),t()(),e(11,"div"),n(12,"i",8),t()(),e(13,"div",9)(14,"span",10),i(15,"239\u066C022\u066C587"),t(),e(16,"span",11),i(17,"\u0631\u06CC\u0627\u0644"),t()(),e(18,"div",12)(19,"span",13),i(20,"\u06A9\u06CC\u0641 \u067E\u0648\u0644\u060C \u0648\u0627\u0645 \u0648 \u0627\u0639\u062A\u0628\u0627\u0631"),t(),e(21,"span"),n(22,"i",14),t()()()()())},styles:[`.hub-header[_ngcontent-%COMP%]{width:100%;height:266px;background-image:linear-gradient(120deg,#ae2ce2,#3d24ca,#44e2e2)}@media screen and (width >= 1200px){.hero-carousel[_ngcontent-%COMP%]   .swiper-slide[_ngcontent-%COMP%], .hero-carousel[_ngcontent-%COMP%]   .swiper-slide[_ngcontent-%COMP%]   img[_ngcontent-%COMP%]{width:748px!important}.hero-carousel[_ngcontent-%COMP%]   swiper-container[_ngcontent-%COMP%]{width:748px!important}.hero-carousel[_ngcontent-%COMP%]   app-custom-swiper[_ngcontent-%COMP%]{display:block;width:748px!important}.hero-carousel[_ngcontent-%COMP%]   .swiper-initialized[_ngcontent-%COMP%]{width:748px!important}}
+/*# sourceMappingURL=services.component-IUZPLORW.css.map */`],changeDetection:0})}return o})();export{g as ServicesComponent};
+//# sourceMappingURL=chunk-V4GU37NN.js.map
