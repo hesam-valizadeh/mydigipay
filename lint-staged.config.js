3c6d4fc44c9ba219adc33c2414f9840cc6c7eef4
Hesam Valizadeh <hesam.valizadeh@gmail.com>
2026-05-16 20:02:53 +0330

    feat: add

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-	'**/*.{ts,tsx}': ['eslint --fix --cache ', 'eslint'],
-	'**/*.{scss,css}': ['stylelint --fix --cache', 'stylelint'],
+'**/*.{scss,css}': () => {
+    return [
+      'stylelint --fix --cache --allow-empty-input "src/**/*.{scss,css}"',
+      'stylelint --allow-empty-input "src/**/*.{scss,css}"'
+    ];
+	},
 	'**/*.{html,json,md,yml}': ['prettier --write', 'prettier --check'],
+	'**/*.{ts,tsx}': ['eslint --fix --cache ', 'eslint'],
 };
