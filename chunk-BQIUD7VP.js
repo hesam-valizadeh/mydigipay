a63f82a13c65ea01d0e10fb4d9cc3e35ba0cc958
hesam-valizadeh <hesam-valizadeh@users.noreply.github.com>
2026-06-02 13:02:27 +0000

    deploy: 7cb43f4718677027633ebe4c8b4b659579bd6fb8

---
+++ chunk-BQIUD7VP.js
@@ -0,0 +1,2 @@
+import{Pa as n}from"./chunk-GC7NBH4Q.js";var p=(()=>{class e{trackEffect(){}static \u0275fac=function(t){return new(t||e)};static \u0275cmp=n({type:e,selectors:[["app-wealth"]],decls:0,vars:0,template:function(t,a){},styles:["/*# sourceMappingURL=wealth.component-KYTEG5ZD.css.map */"],changeDetection:0})}return e})();export{p as WealthComponent};
+//# sourceMappingURL=chunk-BQIUD7VP.js.map
